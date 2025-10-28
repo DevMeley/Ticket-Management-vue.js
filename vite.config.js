050de6a683159d6a6b0f52d5f23cf99ec5914f2b
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-10-28 01:58:02 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+   server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ['ticket-management-react-js.onrender.com'],
+  },
 })
